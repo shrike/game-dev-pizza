860f65c5102ce35be3051f2db8a4fcd8041b1ff2
Teodora Dobreva <tedigdobreva@gmail.com>
2018-08-08 20:26:26 +0300

    Restart game with key press

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -4,7 +4,7 @@
 export default class GameOver extends Phaser.State {
 
   preload() {
-    const gameOverText = 'GAME OVER\nClick to restart';
+    const gameOverText = 'GAME OVER\nPress any key to restart';
     const textStyle = {font: '50px Arial', fill: '#F2F2F2', align: 'center'};
 
     this.stateText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, gameOverText, textStyle);
@@ -12,11 +12,10 @@ export default class GameOver extends Phaser.State {
     // Position text at the center
     this.stateText.anchor.setTo(0.5);
 
-    this.game.input.onTap.addOnce(this.reloadGame, this);
-  }
-
-  reloadGame() {
-    this.game.state.start('Main');
+    this.game.input.keyboard.onDownCallback = function() {
+      this.game.input.keyboard.onDownCallback = null;
+      this.game.state.start('Main');
+    };
   }
 
   create() {
