1706ec3e9e12235251f301da750cff5d55d1db81
Atanas Stavrev <stavreat@gmail.com>
2018-10-24 21:50:59 +0300

    fix?

---
+++ resources/blue-theme.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="blue-theme" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="blue-theme/blue.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="blue-theme/rocks-bomb.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="blue-theme/rocks.png"/>
+ </tile>
+</tileset>
