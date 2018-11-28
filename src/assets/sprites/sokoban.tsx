979d07ebd42865071433c3c43e60985cde4047ef
Silvia Tzeneva <3305245+stzva@users.noreply.github.com>
2018-11-28 19:35:03 +0200

    Add sprite

---
+++ src/assets/sprites/sokoban.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="sokoban" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="game-dev-pizza/resources/sokoban-theme/background.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="game-dev-pizza/resources/sokoban-theme/metal-box.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="game-dev-pizza/resources/sokoban-theme/wooden-box.png"/>
+ </tile>
+</tileset>
