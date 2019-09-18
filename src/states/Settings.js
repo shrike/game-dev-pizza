5c4be6888c0a6ff7590887962a816bfeb359f041
Viktor Ivanov <vivanov@documaster.no>
2019-09-18 18:43:20 +0300

    Remember last used nick

--- src/states/Settings.js
+++ src/states/Settings.js
@@ -5,7 +5,13 @@ export default class Settings extends Phaser.State {
     super();
 
     Client.socket.on('joinSuccessful', (player) => {
-      const nickname = prompt('Please enter your nickname', `Player${player.id}`);
+
+      const old = window.localStorage.getItem('nickname') || `Player${player.id}`
+
+      const nickname = prompt('Please enter your nickname', old);
+
+      window.localStorage.setItem('nickname', nickname);
+
       Client.settings({nickname});
 
       this.game.state.start('MainMenu');
